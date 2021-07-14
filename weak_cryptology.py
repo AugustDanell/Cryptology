1e15b995ad643fd8a2881bc5fdd39af2f0f8ea13
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-07-14 16:39:06 +0200

    Updating so space is un-altered

--- weak_cryptology.py
+++ weak_cryptology.py
@@ -4,15 +4,16 @@ def ceasar(secret_key, plaintext, op = "ENCRYPT"):
         secret_key = -secret_key
 
     for i in plaintext:
-        normalized_id, back_into_ascii = 0, ""
+        normalized_id, back_into_ascii = 0, " "
         alphabet_size = 26
 
-        if(i.isupper()):
-            normalized_id = (ord(i)-65 + secret_key)
-            back_into_ascii = chr((normalized_id%alphabet_size)+65)
-        else:
-            normalized_id = (ord(i)-97 + secret_key)
-            back_into_ascii = chr((normalized_id%alphabet_size)+97)
+        if(i.isalpha()):
+            if(i.isupper()):
+                normalized_id = (ord(i)-65 + secret_key)
+                back_into_ascii = chr((normalized_id%alphabet_size)+65)
+            else:
+                normalized_id = (ord(i)-97 + secret_key)
+                back_into_ascii = chr((normalized_id%alphabet_size)+97)
 
         encrypted_text += back_into_ascii
 
