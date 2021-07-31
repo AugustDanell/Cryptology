f9d9406853a4ba1db13066ea1e131e070e5e7e44
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-07-31 15:56:41 +0200

    Adding docstring

--- attacks.py
+++ attacks.py
@@ -1,5 +1,9 @@
 from weak_cryptology import ceasar
 
+''' ceasar_brute_force_attack
+    Naturally the Ceasar cipher is not very powerful simply because it can easily be brute forced, that is we try to crack it by systematically iterate through all possible keys. The Ceasar cipher uses the 26 letters of the alphabet and so what we can do is iterate through every possible key and test it. 
+'''
+
 def ceasar_brute_force_attack(cryptotext):
     key = 0
     inp = "no"
