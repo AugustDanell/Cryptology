9c59a1fbfb1b9181a146ab1955eb776e268131ba
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-07-31 16:00:56 +0200

    Larger Test Suite

--- test suite.py
+++ test suite.py
@@ -1,4 +1,4 @@
-from weak_cryptology import ceasar, vigenere_cypher as vigenere
+from weak_cryptology import ceasar, vigenere_cypher as vigenere, affine_cypher as affine
 from attacks import ceasar_brute_force_attack
 
 plain_text_lower = "abc"
@@ -21,3 +21,13 @@ if(test_ceasar_brute_force):
 assert vigenere([1,2,3],"aa", "ENCRYPT") == "bc"
 assert vigenere([1,2,3], "bc", "DECRYPT") == "aa"
 assert vigenere([1,2],"aaaaaaaa") == "bcbcbcbc"
+big_key = [1,6,1,7,8,3,1,5,7,4,1]
+s = "bigStringisPowerfulandstronghehexd"
+assert vigenere(big_key, vigenere(big_key, s), op="DECRYPT") == s
+
+## Affine Cipher: A linear coefficient a:
+assert affine(0, 1, "aaa") == ceasar(1, "aaa") == vigenere([1], "aaa")
+assert affine(1, 0, "aaa") == affine(0,1,"aaa")
+print(affine(5, 1, "aaa"))
+print(affine(5, 1, "ggg", "DECRYPT"))
+#assert affine(9,7, affine(9,7,"abc"), "DECRYPT") == "abc"
