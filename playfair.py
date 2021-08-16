a2e7caec1d29264cbbcc980195405e5556d72ec4
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-16 13:13:56 +0200

    Inserted docstrings, TODO: finish, of course, and also add main function.

--- playfair.py
+++ playfair.py
@@ -1,7 +1,16 @@
 def split_into_group_of_five(l):
+    ''' split_into_group_of_five
+        As the name suggests, this function takes in a list and splitts it up into groups of five. It can be made to fit into any arbitary size but here it goes up to size 25 because
+        it is specifically tailored to the playfair cipher, that needs 5 rows of 5 letters, omitting the 'q' specifically.
+    '''
+
     return [l[0:5],l[5:10],l[10:15],l[15:20], l[20:]]
 
 def split_into_pairs(l):
+    ''' split_into_pairs
+        Similar to the split_into_groups_of_five function but instead splits it up into pairs. 
+    '''
+
     new_list = []
     for i in range(1, len(l), 2):
         new_list.append(l[i-1:i+1])
@@ -12,6 +21,10 @@ def split_into_pairs(l):
     return new_list
 
 def transform_digraphs(digraph_list):
+    ''' transform_diagraphs
+        Applying the rules on each diagraph as per the playfair cipher instructions.
+    '''
+
     made_transform = False
     # rule 1:
     
